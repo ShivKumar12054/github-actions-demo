cafc8720538cb29fc3e678abd26aae0f44e7d188
Shivakumar M <shivakumar12054@gmail.com>
2023-07-05 12:12:12 +0530

    add simple react app & moved lear-github-action to correct file

---
+++ simpleReactApp/Hello.js
@@ -0,0 +1,5 @@
+class Hello extends React.Component {
+  render() {
+    return <h1>From {this.props.to} to {this.props.from}</h1>;
+  }
+}
---
+++ simpleReactApp/index.js
@@ -0,0 +1,12 @@
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Hello />
+        <Hello to="sambit" from="shiva"/>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
